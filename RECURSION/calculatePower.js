175af473387180d575d0f23cc0a5d39572c2c69c
rambhumukhia <email@masaischool.com>
2021-12-03 10:13:27 +0530

    power of number O(logn)

--- RECURSION/calculatePower.js
+++ RECURSION/calculatePower.js
@@ -7,4 +7,24 @@ function power(x, n) {
   return xn;
 }
 
-console.log(power(2, 4));
+//above code time complexity is O(n) and based on formula x**n=x*x**n-1
+
+// we can also write above code with formula x**n= x**n/2 *x**n/2 if n is even
+//if n is  odd we can write it as x**n= x**n/2 *x**n/2 *n
+
+// time complexity of this will be O(logn)
+
+function power1(x, n) {
+  if (n == 0) {
+    return 1;
+  }
+  xb2 = power(x, n / 2);
+  x = xb2 * xb2;
+
+  if (n % 2 === 1) {
+    x = xb2 * xb2 * n;
+  }
+  return x;
+}
+
+console.log(power(2, 5));
