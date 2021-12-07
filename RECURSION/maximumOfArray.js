237ce33852672de97e2e86a1b41c4d08bcfb50e2
rambhumukhia <email@masaischool.com>
2021-12-07 11:34:54 +0530

    find max element of an array

---
+++ RECURSION/maximumOfArray.js
@@ -0,0 +1,20 @@
+const M = require("minimatch");
+
+function runProgramme(input) {
+  let data = input.split(" ").map(Number);
+  let max = data[0];
+
+  function maximum(arr, n) {
+    if (n === arr.length) {
+      return;
+    }
+    if (arr[n] > max) {
+      max = arr[n];
+    }
+    maximum(arr, n + 1);
+  }
+  maximum(data, 1);
+  console.log(max);
+}
+
+runProgramme(`-45 -60 -12 -78 -31 -56 -2`);
