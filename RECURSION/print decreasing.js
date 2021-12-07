f8713658db76150889b4bfc8f9b2b1ba3f902f74
rambhumukhia <email@masaischool.com>
2021-12-07 13:02:18 +0530

    print a number in decreasing order from given number till 1

--- RECURSION/print decreasing.js
+++ RECURSION/print decreasing.js
@@ -1,5 +1,4 @@
-//print number from 5 to 1 using recursion
-//print a number in decreasing order from given number
+//print a number in decreasing order from given number till 1
 
 function print(n) {
   if (n === 0) {
@@ -9,4 +8,4 @@ function print(n) {
   print(n - 1);
 }
 
-print(8);
+print(5);
