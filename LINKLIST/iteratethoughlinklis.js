2626e582306b85d959d7efea9c3a928f396bc9ee
mukhiarambhu <rambhumukhia@gmail.com>
2022-01-02 14:55:59 +0530

    print linklist recursively and iterativly

---
+++ LINKLIST/iteratethoughlinklis.js
@@ -0,0 +1,44 @@
+//create a node class
+
+class Node {
+  constructor(val) {
+    this.val = val; //it is value of current node
+    this.next = null; //it is reference to next node
+  }
+}
+
+//creating new node
+let a = new Node("A");
+let b = new Node("B");
+let c = new Node("C");
+let d = new Node("D");
+
+//setting next line of node
+a.next = b;
+b.next = c;
+c.next = d;
+
+function printLinklist(head) {
+  let arr = "";
+  let current = head;
+  while (current != null) {
+    arr += current.val + " ";
+    current = current.next;
+  }
+  console.log(arr);
+}
+
+printLinklist(a);
+
+//recursilvly iterate through an array
+let res = "";
+let printLinklistRecursively = (head) => {
+  if (head == null) {
+    console.log(res);
+    return;
+  }
+  res += head.val + " ";
+  printLinklistRecursively(head.next);
+};
+
+printLinklistRecursively(a);
