6b1eccd23294fa11060570bd2e70a7860497a2e4
mukhiarambhu <rambhumukhia@gmail.com>
2022-01-10 01:22:39 +0530

    pyramid rightangletriangle reverse

--- PATTERN PRINTING/Pyramid.js
+++ PATTERN PRINTING/Pyramid.js
@@ -8,17 +8,17 @@ print pyramid
 
 let n = 5;
 
-// for (let i = 1; i <= n; i++) {
-//   let result = " ";
-//   for (let j = 1; j <= n; j++) {
-//     if (j <= n - i) {
-//       result += "+";
-//     } else {
-//       result + " *  ";
-//     }
-//   }
-//   console.log(result);
-// }
+for (let i = 1; i <= n; i++) {
+  let result = "";
+  for (let j = 1; j <= n; j++) {
+    if (j < n - i + 1) {
+      result += " ";
+    } else {
+      result += "* ";
+    }
+  }
+  console.log(result);
+}
 
 //right angled triangle
 
@@ -53,6 +53,49 @@ for (let i = 1; i <= n; i++) {
   let result = "";
   for (let j = 1; j <= n; j++) {
     if (j <= n - i + 1) {
+      result += "* ";
+    }
+  }
+  console.log(result);
+}
+
+/*
+print
+    *
+   **
+  ***
+ ****
+*****
+
+*/
+
+for (let i = 1; i <= n; i++) {
+  let result = "";
+  for (let j = 1; j <= n; j++) {
+    if (j < n - i + 1) {
+      result += "  ";
+    } else {
+      result += "* ";
+    }
+  }
+  console.log(result);
+}
+
+/*
+print 
+*****
+ ****
+  ***
+   **
+    *
+*/
+console.log("/////////////");
+for (let i = 1; i <= n; i++) {
+  let result = "";
+  for (let j = 1; j <= n; j++) {
+    if (j <= n - (n - i + 1)) {
+      result += "  ";
+    } else {
       result += "* ";
     }
   }
