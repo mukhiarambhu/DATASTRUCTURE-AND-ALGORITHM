e6e14e894dbd396ea5dc1d9b4fec25328e304a48
mukhiarambhu <rambhumukhia@gmail.com>
2022-01-10 01:00:25 +0530

    right angled trianle pattern

---
+++ PATTERN PRINTING/Pyramid.js
@@ -0,0 +1,60 @@
+/*
+print pyramid
+     *
+    *  *
+   *  *  *
+  *  *  *  *
+*/
+
+let n = 5;
+
+// for (let i = 1; i <= n; i++) {
+//   let result = " ";
+//   for (let j = 1; j <= n; j++) {
+//     if (j <= n - i) {
+//       result += "+";
+//     } else {
+//       result + " *  ";
+//     }
+//   }
+//   console.log(result);
+// }
+
+//right angled triangle
+
+/*
+ *
+ **
+ ***
+ ****
+ */
+
+for (let i = 1; i <= n; i++) {
+  let result = "";
+  for (let j = 1; j <= n; j++) {
+    if (j <= i) {
+      result += "* ";
+    }
+  }
+  console.log(result);
+}
+
+/*reverse right triangle;
+ *****
+ ****
+ ***
+ **
+ *
+ */
+
+console.log("////////////////////////////");
+
+for (let i = 1; i <= n; i++) {
+  let result = "";
+  for (let j = 1; j <= n; j++) {
+    if (j <= n - i + 1) {
+      result += "* ";
+    }
+  }
+  console.log(result);
+}
