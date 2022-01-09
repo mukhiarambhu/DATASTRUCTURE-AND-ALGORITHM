560187f390ea14703c2c2c887a70f364a61b158c
mukhiarambhu <rambhumukhia@gmail.com>
2022-01-09 23:43:33 +0530

    print letter E,F C,

--- PATTERN PRINTING/Square.js
+++ PATTERN PRINTING/Square.js
@@ -18,3 +18,63 @@ for (let i = 0; i < n; i++) {
   }
   console.log(res);
 }
+
+/*
+print this pattern (c)
+*****
+*   
+*   
+*****   
+*/
+
+for (let i = 0; i < n; i++) {
+  result = "";
+  for (let j = 0; j < n; j++) {
+    if (i == 0 || j == 0 || i == n - 1) {
+      result += "* ";
+    } else {
+      result += " ";
+    }
+  }
+  console.log(result);
+}
+
+/*
+print this pattern (E)
+*****
+*
+*****  
+*   
+*****   
+*/
+let a = 5;
+
+for (let i = 0; i < a; i++) {
+  let result = "";
+  for (let j = 0; j < a; j++) {
+    if (i == 0 || j == 0 || i == a - 1 || i == Math.ceil(a / 2) - 1) {
+      result += "* ";
+    }
+  }
+  console.log(result);
+}
+
+/*
+print this pattern (E)
+*****
+*
+***** 
+* 
+*  
+*****   
+*/
+
+for (let i = 0; i < a; i++) {
+  let result = "";
+  for (let j = 0; j < a; j++) {
+    if (i == 0 || j == 0 || i == Math.ceil(a / 2) - 1) {
+      result += "* ";
+    }
+  }
+  console.log(result);
+}
