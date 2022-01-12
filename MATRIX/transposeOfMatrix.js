017165e7fd6e2a807b3001f7456dcf937ff86b13
mukhiarambhu <rambhumukhia@gmail.com>
2022-01-12 15:03:47 +0530

    find transpose of a matrix

---
+++ MATRIX/transposeOfMatrix.js
@@ -0,0 +1,20 @@
+//Transpose of a given matrix
+function runProgramme(input) {
+  let newInput = input.split("\n");
+  let mat = newInput.splice(0).map((row) => {
+    return row.split(" ").map(Number);
+  });
+  let r = mat.length;
+  let c = mat[0].length;
+
+  for (let i = 0; i < c; i++) {
+    let result = "";
+    for (let j = 0; j < r; j++) {
+      result += mat[j][i] + " ";
+    }
+    console.log(result);
+  }
+}
+
+runProgramme(`1 2 3
+4 5 6`);
