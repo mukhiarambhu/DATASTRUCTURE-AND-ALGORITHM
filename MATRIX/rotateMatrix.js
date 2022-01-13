ad90e785e696658d7325b864a06b19cd432b57a3
mukhiarambhu <rambhumukhia@gmail.com>
2022-01-13 15:23:05 +0530

    rotate arry by 90 degree clockwise and anticlockwise

--- MATRIX/rotateMatrix.js
+++ MATRIX/rotateMatrix.js
@@ -1,25 +1,43 @@
-//rotate from top to bottom
-//traverse an array
-// 1 8 9
-// 2 7 10
-// 3 6 11   ===> 4 3 2 15 6 7 8 12 11 10 9
-// 4 5 12
+// //rotate the matrix by 90  anticlockwise
+// function runProgramme(input) {
+//   let data = input.split("\n").map((row) => {
+//     return row.split(" ").map(Number);
+//   });
 
+//   let r = data.length;
+//   let c = data[0].length;
+
+//   for (let i = c - 1; i >= 0; i--) {
+//     let res = "";
+//     for (let j = 0; j < r; j++) {
+//       res += data[j][i] + " ";
+//     }
+//     console.log(res);
+//   }
+// }
+// runProgramme(`1 2 3 4
+// 5 6 7 8
+// 1 2 3 4
+// 5 6 7 8`);
+
+//rotate the matrix by 90  clockwise
 function runProgramme(input) {
   let data = input.split("\n").map((row) => {
     return row.split(" ").map(Number);
   });
+
   let r = data.length;
   let c = data[0].length;
-  let res = "";
+
   for (let i = 0; i < c; i++) {
+    let res = "";
     for (let j = r - 1; j >= 0; j--) {
       res += data[j][i] + " ";
     }
+    console.log(res);
   }
-  console.log(res);
 }
-runProgramme(`1 8 9
-2 7 10
-3 6 11
-4 5 12`);
+runProgramme(`1 2 3 4
+5 6 7 8
+1 2 3 4
+5 6 7 8`);
